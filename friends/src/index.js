5eec5a7e2254b0bcafee1ae7cf2cc4559e3fdad6
LadyKerr <kadiekerr@gmail.com>
2019-06-19 18:27:42 -0400

    initial setUp, store & reducer

---
+++ friends/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import friendsReducer from "./reducers";
+import "./index.css";
+import App from "./App";
+
+const store = createStore(
+  friendsReducer,
+  applyMiddleware(thunk, logger)
+)
+
+ReactDOM.render(
+<Provider state={store}>
+  <App />
+</Provider>,
+  document.getElementById('root')
+  );
+
+
